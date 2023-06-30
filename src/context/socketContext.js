91bc362b4a9b04382a47814d1d7d20b26d6e3d7c
mgregerson <mgregerson4321@gmail.com>
2023-06-30 16:19:22 -0400

    Added error handling to socket context

--- src/context/socketContext.js
+++ src/context/socketContext.js
@@ -5,15 +5,30 @@ export const SocketContext = React.createContext();
 
 export const SocketProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const newSocket = io("http://localhost:3001");
-    setSocket(newSocket);
+
+    newSocket.on("connect", () => {
+      setSocket(newSocket);
+      setLoading(false);
+    });
+
+    newSocket.on("connect_error", (error) => {
+      // Handle connection error
+      console.error("Socket connection error:", error);
+      setLoading(false);
+    });
 
     return () => {
       newSocket.disconnect();
     };
   }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
