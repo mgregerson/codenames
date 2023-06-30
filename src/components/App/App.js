f4a480cec51bd591048979a6f366aea9ccfa3b1c
mgregerson <mgregerson4321@gmail.com>
2023-06-30 10:25:00 -0400

    Refactor code for player registration and event handling
    In this commit:
    
    Refactored the code for player registration and event handling in the App component.
    Created state variables for player, players, teams, and guesses using the useState hook.
    Implemented event listeners for player joining, team updates, player leaving, and guess made events using the socket.on method.
    Updated the handlePlayerRegistration function to correctly set the player and update the players state.
    Updated the filter function in the handlePlayerRegistration function to compare the player.name with data.playerName.
    Renamed the name parameter to playerName in the handleTeamSelect function to match the parameter name used in the handlePlayerRegistration function.
    Updated the socket.emit calls to pass the correct parameters in the handleTeamSelect and handleMakeGuess functions.
    Added a cleanup function in the useEffect hook to disconnect the socket on component unmount.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,83 @@
 import "./App.css";
-import codenamesApi from "../../utils/api";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import React from "react";
 import CardList from "../CardList/CardList.tsx";
+import { useCardContext } from "../../context/cardContext";
+import { SocketContext } from "../../context/socketContext";
+import TeamSelect from "../PlayerRegistration/PlayerRegistration";
+import PlayerRegistration from "../PlayerRegistration/PlayerRegistration";
 
 function App() {
-  const [cards, setCards] = useState(null);
-  const [gameStart, setGameStart] = useState(false);
+  const cards = useCardContext();
+  const socket = useContext(SocketContext);
+  const [player, setPlayer] = useState({});
+  const [players, setPlayers] = useState([]);
+  const [teams, setTeams] = useState([]);
+  const [guesses, setGuesses] = useState([]);
+
+  console.log("THE PLAYER IN APP IS", player);
+  console.log("THE PLAYERS IN APP IS", players);
+  console.log("THE TEAMS IN APP IS", teams);
 
   useEffect(() => {
-    async function getCards() {
-      const cards = await codenamesApi.getCardsForGame();
-      setCards(cards);
-    }
-    getCards();
-  }, []);
+    // Handle player joining event
+    socket.on("playerJoined", (newPlayerName) => {
+      setPlayers((prevPlayers) => [...prevPlayers, newPlayerName]);
+    });
 
-  console.log(cards);
+    socket.on("teamUpdate", (redTeam, blueTeam) => {
+      setTeams({ redTeam, blueTeam });
+    });
 
-  return <div className="App">{cards && <CardList cards={cards} />}</div>;
+    // Handle player leaving event
+    // Handle player leaving event
+    socket.on("playerLeft", (playerName) => {
+      setPlayers((prevPlayers) =>
+        prevPlayers.filter((name) => name !== playerName)
+      );
+    });
+
+    // Handle guess made event
+    socket.on("guessMade", (guess) => {
+      setGuesses((prevGuesses) => [...prevGuesses, guess]);
+    });
+
+    // Cleanup socket connection on unmount
+    return () => {
+      socket.disconnect();
+    };
+  }, [socket]);
+
+  const handleJoinGame = () => {
+    // Emit join event to the server
+    socket.emit("join", player);
+  };
+
+  function handlePlayerRegistration(data) {
+    setPlayer(data);
+    setPlayers((prevPlayers) =>
+      prevPlayers.filter((player) => player.name !== data.playerName)
+    );
+    handleTeamSelect(data.team, data.role, data.playerName);
+    console.log(data);
+  }
+
+  const handleTeamSelect = (team, role, name) => {
+    // Emit joinTeam event to the server
+    socket.emit("joinTeam", team, role, name);
+  };
+
+  const handleMakeGuess = (guess) => {
+    // Emit makeGuess event to the server
+    socket.emit("makeGuess", guess);
+  };
+
+  return (
+    <div className="App">
+      <PlayerRegistration handlePlayerRegistration={handlePlayerRegistration} />
+      {cards && <CardList cards={cards} />}
+    </div>
+  );
 }
 
 export default App;
