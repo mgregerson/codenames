5c92bf19f07303d3c6a97d56b0fd6376e35d6e2e
mgregerson <mgregerson4321@gmail.com>
2023-07-05 14:31:13 -0400

    Updated handling of a guess in game

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import { Button } from "react-bootstrap";
 import { CardProps } from "../../types/types";
+import { useContext } from "react";
+import { SocketContext } from "../../context/socketContext";
 
 /**
  * Card component.
@@ -10,19 +12,29 @@ import { CardProps } from "../../types/types";
  */
 
 function Card(props: CardProps) {
-  const { word, team } = props;
-  const [selected, setSelected] = useState<boolean>(false);
+  const { word, team, currTeam } = props;
+  const [selectedCard, setSelectedCard] = React.useState<boolean>(false);
+  const socket = useContext(SocketContext);
+
+  useEffect(() => {
+    // Handle making a guess
+    socket.on("updateSelected", (chosenWord: any) => {
+      if (chosenWord.word === word) {
+        setSelectedCard(true);
+      }
+    });
+  }, [socket, word]);
 
   const handleClick = (): void => {
-    if (!selected) {
-      setSelected(true);
-      props.handleGuess(word, team);
+    if (!selectedCard) {
+      socket.emit("makeGuess", word, currTeam);
+      setSelectedCard(true);
     }
   };
 
   return (
-    <div className={`Card ${selected ? "selected" : ""}`}>
-      <Button variant={team} disabled={selected} onClick={handleClick}>
+    <div className={`Card ${selectedCard ? "selected" : ""}`}>
+      <Button variant={team} disabled={selectedCard} onClick={handleClick}>
         {word.toUpperCase()}
       </Button>
     </div>
