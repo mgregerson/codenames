5818b071870ea648be1e4ed180371beafbec1870
mgregerson <mgregerson4321@gmail.com>
2023-07-10 19:27:06 -0400

    Added dark-red and dark-blue custom colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      blue: {
+        light: "#85d7ff",
+        DEFAULT: "#1fb6ff",
+        dark: "#3284A3",
+      },
+      red: {
+        light: "#ff7ce5",
+        DEFAULT: "#ff49db",
+        dark: "#8F2B1C",
+      },
+      background: "#8F2B1C",
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
