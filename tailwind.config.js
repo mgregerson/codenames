762d94c9f37720bd1cd4388a1e05121c8d5d5705
mgregerson <mgregerson4321@gmail.com>
2023-07-11 12:43:50 -0400

    Updated tailwind config file with new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
@@ -14,6 +16,15 @@ module.exports = {
         dark: "#8F2B1C",
       },
       background: "#8F2B1C",
+      brown: "#EED5B2",
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
     },
     extend: {},
   },
