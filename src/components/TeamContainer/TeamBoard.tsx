6e21e3e1edf63cee145fe71b4664672c45dc6a22
mgregerson <mgregerson4321@gmail.com>
2023-07-11 16:48:31 -0400

    Added styling to TeamContainer - Scoring is larger, player names displayed with grid display

--- src/components/TeamContainer/TeamBoard.tsx
+++ src/components/TeamContainer/TeamBoard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import TeamPlayer from "./TeamList/TeamPlayer/TeamPlayer";
 import TeamScore from "./TeamList/TeamScore/TeamScore";
 import { TeamBoardProps } from "../../types/types";
+import "./TeamBoard.css";
 import redGuess from "../../styles/redGuess.png";
 import blueGuess from "../../styles/blueGuess.png";
 /** TeamList Component
@@ -23,16 +24,19 @@ export default function TeamBoard({
   const cardImage = teamColor === "red" ? redGuess : blueGuess;
 
   return (
-    <div id={`TeamBoard-${teamColor}`} className={`bg-${teamColor}-dark`}>
-      <div className="w-full box-border flex items-center justify-between">
-        <div className="w-1/2">
-          <img src={cardImage} alt={teamColor} className="h-auto max-w-full" />
+    <div
+      id={`TeamBoard-${teamColor}`}
+      className="rounded-lg p-2 mt-10 mx-2 mb-6 grid grid-rows-[auto,1fr] shadow-xl border-1 border-black"
+    >
+      <div className="box-border flex items-center justify-between">
+        <div className="w-3/4">
+          <img src={cardImage} alt={teamColor} className="rounded-xl" />
         </div>
-        <div className="w-1/2">
+        <div className="w-1/4 text-white font-extrabold text-5xl">
           {teamScore && <TeamScore score={teamScore} />}
         </div>
       </div>
-      <div className="flex flex-wrap justify-center">
+      <div className="flex flex-wrap mt-2">
         {players.map((player, index) => (
           <TeamPlayer key={index} name={player.name} />
         ))}
